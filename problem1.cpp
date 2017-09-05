8c05edd99d16af831a039104057dfc3e6edc624d
ManishaSahu6 <31317939+ManishaSahu6@users.noreply.github.com>
2017-09-05 22:56:19 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,15 @@
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+    	int num1,num2;
+    	cout<<"Enter first number\t";
+    	cin>>num1;
+    	cout<<"Enter second number\t";
+    	cin>>num2;
+    	if (num1>num2)
+    	cout<<num1<<"\t is maximum";
+    	else
+    	cout<<num2<<"\t is maximum";
+    	return 0;
+    }
